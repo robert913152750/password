94c9d4ca43b97d3ea8c7909f4988d579e7dba62a
Robert Cai <robert913152750@gmail.com>
2018-09-29 23:28:17 +0800

    use while i > 0

--- pwdteacher.py
+++ pwdteacher.py
@@ -1,15 +1,19 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+	i = i -1
 	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('登入成功!')
 		break
 	else:
-		i = i -1
-		print('錯誤，剩餘',i,'機會')
-		if i == 0:
-			print('系統關閉')
-			break
+		print('密碼錯誤')
+		if i > 0:
+			print('剩餘',i,'機會')
+		
+		
+
+
+		
 
 
